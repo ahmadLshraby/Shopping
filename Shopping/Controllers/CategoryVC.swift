e96f0b94832144519310f610e9d2fb0ebb387c5b
Ahmad Shraby <ahmadelshraby10@outlook.com>
2019-02-13 00:40:53 +0200

    UI
    Navigation Controller
    TableView
    Start the UI

---
+++ Shopping/Controllers/CategoryVC.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Shopping
+//
+//  Created by sHiKoOo on 2/13/19.
+//  Copyright © 2019 sHiKoOo. All rights reserved.
+//
+
+import UIKit
+
+class CategoryVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var categoryTable: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+
+
+}
+
---
+++ Shopping/View/CategoryCell.swift
@@ -0,0 +1,28 @@
+//
+//  CategoryCell.swift
+//  Shopping
+//
+//  Created by sHiKoOo on 2/13/19.
+//  Copyright © 2019 sHiKoOo. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+    
+    @IBOutlet weak var categoryImg: UIImageView!
+    @IBOutlet weak var categoryLbl: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
