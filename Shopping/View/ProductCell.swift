1b93bb914a9a94ae67941576eaca4226ef6ef623
Ahmad Shraby <ahmadelshraby10@outlook.com>
2019-02-13 01:58:31 +0200

    UI
    Collection View build

---
+++ Shopping/View/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  Shopping
+//
+//  Created by sHiKoOo on 2/13/19.
+//  Copyright © 2019 sHiKoOo. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImg: UIImageView!
+    @IBOutlet weak var titleLbl: UILabel!
+    @IBOutlet weak var priceLbl: UILabel!
+    
+}
