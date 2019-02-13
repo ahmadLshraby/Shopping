2e32e646d3c24f9e7d8ef94e51ef67af70e13d77
Ahmad Shraby <ahmadelshraby10@outlook.com>
2019-02-13 02:23:13 +0200

    Data models and services
    ProductModel
    add data of products to data service
    build functions for products to return products depending on the category title

---
+++ Shopping/Model/Product.swift
@@ -0,0 +1,22 @@
+//
+//  Product.swift
+//  Shopping
+//
+//  Created by sHiKoOo on 2/13/19.
+//  Copyright © 2019 sHiKoOo. All rights reserved.
+//
+
+import Foundation
+
+struct Product {
+    
+    private(set) public var title: String
+    private(set) public var price: String
+    private(set) public var imageName: String
+    
+    init(title: String, price: String, imageName: String) {
+        self.title = title
+        self.price = price
+        self.imageName = imageName
+    }
+}
--- Shopping/Services/DataService.swift
+++ Shopping/Services/DataService.swift
@@ -20,9 +20,63 @@ class DataServices {
         Category(title: "ELECTRONICS", imageName: "digital")
     ]
     
+    //set data source we want to show it for products
+    private let shirts = [
+        Product(title: "Shirt no.1", price: "10$", imageName: "shirt01"),
+        Product(title: "Shirt no.2", price: "20$", imageName: "shirt02"),
+        Product(title: "Shirt no.3", price: "30$", imageName: "shirt03"),
+        Product(title: "Shirt no.4", price: "40$", imageName: "shirt04"),
+        Product(title: "Shirt no.5", price: "50$", imageName: "shirt05")
+    ]
+    private let hoodies = [
+        Product(title: "Hoodies no.1", price: "10$", imageName: "hoodie01"),
+        Product(title: "Hoodies no.2", price: "20$", imageName: "hoodie02"),
+        Product(title: "Hoodies no.3", price: "30$", imageName: "hoodie03"),
+        Product(title: "Hoodies no.4", price: "40$", imageName: "hoodie04"),
+        ]
+    private let hats = [
+        Product(title: "hats no.1", price: "10$", imageName: "hat01"),
+        Product(title: "hats no.2", price: "20$", imageName: "hat02"),
+        Product(title: "hats no.3", price: "30$", imageName: "hat03"),
+        Product(title: "hats no.4", price: "40$", imageName: "hat04")
+    ]
+    private let electronics = [Product]()
+    
+    
+    
     func getCategories() -> [Category] {
         return categories
     }
+    
+    
+    func getShirts() -> [Product] {
+        return hats
+    }
+    func getHoodies() -> [Product] {
+        return hoodies
+    }
+    func getHats() -> [Product] {
+        return hats
+    }
+    func getElectronics() -> [Product] {
+        return electronics
+    }
+    
+    
+    func getProducts(forCategoryTitle title: String) -> [Product] {
+        switch title {
+        case "SHIRTS":
+            return  getShirts()
+        case "HOODIES":
+            return  getHoodies()
+        case "HATS":
+            return getHats()
+        default:
+            return getShirts()
+        }
+    }
+    
+    
     
     
 }
